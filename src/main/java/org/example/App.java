d35730e40efccc239555b961f2721f1a2dcfe934
nicol <nicol.15ar@outlook.com>
2024-05-08 11:03:27 -0500

    cambio

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import org.example.models.Nissan;
+import org.example.models.Toyota;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+        //Nissan nis = new Nissan("PAL202");
+        //System.out.println("VElocidad Maxima Nis: " + nis.getVelocidadMaxima());
+
+
+        Toyota tot = new Toyota("PAL202");
+        System.out.println("VElocidad Maxima Toy: " + tot.getVelocidadMaxima());
+    }
+}
---
+++ src/main/java/org/example/models/Nissan.java
@@ -0,0 +1,10 @@
+package org.example.models;
+
+public class Nissan extends Auto {
+
+
+    public Nissan(String nroPlaca) {
+        super(nroPlaca);
+        setVelocidadMaxima(300);
+    }
+}
